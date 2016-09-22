a61b8e9f81feb4122576dc83af8a32927042fb5a
AzadN <azadnautiyal54.an@gmail.com>
2016-09-22 22:53:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,6 @@ var button=document.getElementById('submit_btn');
   request.send(null);
 };*/
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 
 submit.onclick=function()
@@ -41,6 +39,8 @@ submit.onclick=function()
           }
       }
   };
+  var nameInput=document.getElementById('name');
+var name=nameInput.value;
   request.open('GET','http://azadn.imad.hasura-app.io/submit-name=' + name,true);
   request.send(null);
 };
