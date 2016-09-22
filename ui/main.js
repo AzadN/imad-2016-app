9f505b9f0025ab4aca2c7a3159f00e44c9441e13
AzadN <azadnautiyal54.an@gmail.com>
2016-09-22 22:28:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,13 +8,14 @@ button.onclick=function()
       {
           if(request.status===200)
           {
-              var counter=request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML=counter.toString();
+               var names=['name1','name2','name3','name4'];
+               var list='';
+               for(var i=0;i<names.length;i++)
+               list+='<li>'+names[i]+'</li>';
           }
       }
   };
-  request.open('GET','http://azadn.imad.hasura-app.io/counter',true);
+  request.open('GET','http://azadn.imad.hasura-app.io/submit-name'+name,true);
   request.send(null);
 };
 
@@ -23,7 +24,8 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
-   var names=['name1','name2','name3','name4'];
+   var names=request.responseText;
+   names=JSON.parse(names);
    var list='';
    for(var i=0;i<names.length;i++)
    list+='<li>'+names[i]+'</li>';
