136ce76552cea155ffc9b45c1ffd72fa423c6688
AzadN <azadnautiyal54.an@gmail.com>
2016-09-22 19:11:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,7 @@
-console.log('Loaded!');
-
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
-
-var img=document.getElementById('x');
-var marginLeft=0;
-function moveRight()
+var button=document.getElementByID('counter');
+button.onclick=function()
 {
-    marginLeft=marginLeft+11;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,50);
+  counter+=1;
+  var span=document.getElementByID('count');
+  span.InnerHTML=counter.toString();
 };
