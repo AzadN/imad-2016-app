bb87beb915d740afca3fd77572d443ac532edb92
AzadN <azadnautiyal54.an@gmail.com>
2016-11-09 21:54:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/create-user',function(req,res){
           res.status(500).send(err.toString());
       }
       else{
-          res.send(JSON.stringify(result.rows));
+          res.send('User '+username+' successfully created');
       }
     });
 });
